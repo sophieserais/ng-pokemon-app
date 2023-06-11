722783c1f9bb8718598f7b26779962492d812ebc
Sophie Serais <seraiss@Sophies-MacBook-Pro.local>
2023-06-11 12:11:05 +0200

    les composants

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,18 @@
-import { Component } from "@angular/core"
+import { Component, OnInit } from "@angular/core"
+import { POKEMONS } from "./mock-pokemon-list"
+import { Pokemon } from "./pokemon"
 
 @Component({
 	selector: "app-root",
-	template: ` <h1>Welcome to {{ title }}!</h1> `,
+	templateUrl: "app.component.html",
 })
-export class AppComponent {
-	title = "ng-pokemon-app"
+export class AppComponent implements OnInit {
+	pokemonList = POKEMONS
+	ngOnInit(): void {
+		console.table(this.pokemonList)
+		this.selectPokemon(this.pokemonList[1])
+	}
+	selectPokemon(pokemon: Pokemon) {
+		console.log(`Vous avez clique sur le ${pokemon.name}`)
+	}
 }
